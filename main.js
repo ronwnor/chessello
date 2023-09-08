c02c2159907614f9dd62eb6bf29bf0a6dadd2b71
ronwnor <109063662+ronwnor@users.noreply.github.com>
2023-09-08 15:22:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const $  = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-const {floor, abs} = Math;
+const {floor, abs, max} = Math;
+const mod = (n, k) => n - k*floor(n/k); 
+const isModalShowing = false;
 
 class Vec {
     constructor(x,y){
@@ -61,33 +63,144 @@ let cells = [
 ];
 let turn = 1;
 let candidates = [];
-let selected;
+let selected, dragged;
+const timeFormat = Vec.new(NaN, NaN);
+const timeLeft = Vec.new(NaN, NaN);
+let hasGameStarted = false;
 
 const boardEl = $`.board`;
+const dragEl = $`#follow-mouse`;
 const [blackDiskEl, whiteDiskEl] = $`.disks`.children;
+const [blackClockEl, whiteClockEl] = $$('.clock');
+
 
 // initialize tiles
 for(let i=0; i<64; i++){
     let tile = document.createElement('div');
     tile.id = 'tile'+(i%8 + 56 - 8*floor(i/8));
     tile.className = 'tile ' +  ((floor(i/8) + i)%2 ? 'light ' : 'dark ');
-    tile.onclick = handleTileClick;
+    tile.onmousedown = handleTileMouseDown;
+    tile.onmouseup   = handleTileMouseUp;
+
+    tile.ontouchstart = (e) => {
+        e.preventDefault(); 
+        handleTileMouseDown(e.touches[0]); 
+    }
+    tile.ontouchend = (e) => { 
+        e.preventDefault();
+        let target = document.elementFromPoint(32+ +dragEl.style.left.slice(0,-2), 32+ +dragEl.style.top.slice(0,-2));
+        if(target.classList.contains('tile')){
+            handleTileMouseUp({target});
+        } 
+    }
     boardEl.appendChild(tile);
 }
 
-whiteDiskEl.onclick = handleWhiteDiskClick;
-blackDiskEl.onclick = handleBlackDiskClick;
+whiteDiskEl.onmousedown = handleWhiteDiskMouseDown;
+blackDiskEl.onmousedown = handleBlackDiskMouseDown;
+whiteDiskEl.onmouseup = blackDiskEl.onmouseup = handleDiskMouseUp;
+
+whiteDiskEl.ontouchstart = (e) => {
+    e.preventDefault();
+    handleWhiteDiskMouseDown();
+}
+blackDiskEl.ontouchstart = (e) => {
+    e.preventDefault();
+    handleBlackDiskMouseDown();
+}
+
+whiteDiskEl.ontouchend = blackDiskEl.ontouchend = (e) => {
+    e.preventDefault();
+    let target = document.elementFromPoint(32+ +dragEl.style.left.slice(0,-2), 32+ +dragEl.style.top.slice(0,-2));
+    if(target.classList.contains('tile')){
+        handleTileMouseUp({target});
+    } 
+}
+
+
+addEventListener('mousemove', (e) => { 
+    dragEl.style.left = e.x - 32 + 'px'; 
+    dragEl.style.top  = e.y - 32 + 'px';
+}, {passive: false});
+
+addEventListener('touchmove', (e) => { 
+    e.preventDefault();
+    e = e.touches[0];
+    dragEl.style.left = e.clientX - 32 + 'px'; 
+    dragEl.style.top  = e.clientY - 32 + 'px';
+    console.log('move:', e.clientX, e.clientY, document.elementFromPoint(e.clientX, e.clientY));
+}, {passive: false});
+
+onmouseup = (e) => {
+    if(e.target.tagName != 'BODY' && !e.target.classList.contains('disks')) return;
+    dragEl.style.backgroundImage = '';
+    dragged && (dragged.style.backgroundSize = 'cover');
+    boardEl.childNodes.forEach(e => e.classList.remove('mid-drag'));
+    dragged = undefined;
+    selected = undefined;
+    candidates = [];
+    updateDisplay();
+}
+// handleTouchend = (e) => {
+//     // e.preventDefault();
+//     onmouseup(e.touches[0]);
+// }
+
+document.body.onclick = (e) => { if(e.target.tagName == 'BODY'){selected = undefined; candidates = []; updateDisplay(); }}
+
+
+let t0 = 0;
+(function clockHandler(t){
+
+    let dt = t - t0;
+    t0 = t;
+
+    if(hasGameStarted){
+        timeLeft[turn == 1 ? 'x' : 'y'] -= dt;
+        updateClockDisplay();
+    }
+
+    requestAnimationFrame(clockHandler);
+})();
+
+function formatTime(t){
+
+    t = max(t, 0);
+
+    const min = ('0'+(mod(t/60000, 60)|0)).slice(-2);
+    const sec = ('0'+(mod(t/1000 , 60)|0)).slice(-2);
+    const ms  = ('0'+(mod(t/10   ,100)|0)).slice(-2);
+    
+    let str =  min + ':' + sec + (!+min && +sec < 20 ? ':' + ('0'+ms).slice(-2) : '');
+
+    return str;
+}
+
+function updateClockDisplay(){
+    tWhite = timeLeft.x;
+    tBlack = timeLeft.y;
+    whiteClockEl.innerText = formatTime(tWhite);
+    blackClockEl.innerText = formatTime(tBlack);
+
+    if(tWhite < 0){
+        showEndModal('Black', 'Timeout');
+    } else if(tBlack < 0){
+        showEndModal('White', 'Timeout');
+    }
+
+
+}
 
 function updateDisplay(){
     for(let i=0; i<64; i++){
         let tile = $('#tile' + i);
         
-        tile.innerText = candidates.includes(i) ? '●' : '';
+        tile.classList.remove('black', 'white', 'disk', 'pawn', 'selected', 'candidate');
         
-        tile.classList.remove('black', 'white', 'disk', 'pawn', 'selected');
-
         if(i == selected) 
             tile.classList.add('selected');
+        else if(candidates.includes(i))
+            tile.classList.add('candidate');
         
         switch(cells[i]){
             case 1: tile.classList.add('white', 'pawn'); break;
@@ -215,6 +328,89 @@ function getFlippableDisks(targetIdx, diskType){
     return allFlippables;
 }
 
+function endTurn(){
+
+    if(!cells.some(e => e == 3)){
+        showEndModal('Black', 'Othello');
+    } else if(!cells.some(e => e == 4)){
+        showEndModal('White', 'Othello');
+    }
+
+    turn = 3 - turn; // 3 - (1) = 2;  3 - (2) = 1;
+    hasGameStarted = true;
+    timeLeft[turn == 1 ? 'y' : 'x'] += timeFormat.y;
+    blackClockEl.classList.toggle('active');
+    whiteClockEl.classList.toggle('active');
+}
+
+function handleTileMouseUp(e){
+    dragEl.style.backgroundImage = '';
+    dragged && (dragged.style.backgroundSize = 'cover');
+    
+    boardEl.childNodes.forEach(e => e.classList.remove('mid-drag'));
+
+    dragged = undefined;
+
+    let target = Number(e.target.id.slice(4));
+
+    if(candidates.includes(target)){
+        if(cells?.[selected] != undefined){
+            cells[target] = cells[selected];
+            cells[selected] = 0;
+        } else {
+            cells[target] = turn + 2;
+        } 
+
+        // also flip disks if there's any 
+        for(let i of getFlippableDisks(target, turn + 2)){
+            cells[i] = 7 - cells[i]; // 7-(3) = 4;  7-(4) = 3;
+        }
+
+        endTurn();
+
+        selected = undefined;
+        candidates = [];
+        updateDisplay();
+    }
+}
+
+function handleTileMouseDown(e){
+    let target = Number(e.target.id.slice(4));
+    
+    if(cells[target] == turn){
+        dragEl.style.backgroundImage = `var(--${turn == 1 ? 'white' : 'black'}-pawn)`;
+        dragged = e.target;
+        dragged.style.backgroundSize = '0';
+        boardEl.childNodes.forEach(e => e.classList.add('mid-drag'));
+        
+        selected = target;
+        candidates = cells.map((_,i) => i).filter((i) => canPawnMoveTo(selected, i));
+        
+        // console.log(selected, candidates);
+    } else {
+
+        if(candidates.includes(target)){
+            if(cells?.[selected] != undefined){
+                cells[target] = cells[selected];
+                cells[selected] = 0;
+            } else {
+                cells[target] = turn + 2;
+            } 
+
+            // also flip disks if there's any 
+            for(let i of getFlippableDisks(target, turn + 2)){
+                cells[i] = 7 - cells[i]; // 7-(3) = 4;  7-(4) = 3;
+            }
+
+            endTurn();
+        }
+
+
+        selected = undefined;
+        candidates = [];
+    }
+    updateDisplay();
+}
 function handleTileClick(e){
     let target = Number(e.target.id.slice(4));
     
@@ -236,10 +432,11 @@ function handleTileClick(e){
 
             // also flip disks if there's any 
             for(let i of getFlippableDisks(target, turn + 2)){
-                cells[i] = 7 - cells[i]; // 6-(2) = 4;  6-(4) = 2;
+                cells[i] = 7 - cells[i]; // 7-(3) = 4;  7-(4) = 3;
             }
 
-            turn = 3 - turn; // 3 - (1) = 2;  3 - (2) = 1;
+            endTurn();
+
         }
 
 
@@ -248,9 +445,10 @@ function handleTileClick(e){
     }
     updateDisplay();
 }
-
-function handleWhiteDiskClick(){
+function handleWhiteDiskMouseDown(){
     if(turn == 1){
+        dragEl.style.backgroundImage = `var(--white-disk)`;
+        boardEl.childNodes.forEach(e => e.classList.add('mid-drag'));
         selected = -1;
         candidates = [];
         for(let i=0; i<64; i++){
@@ -261,8 +459,10 @@ function handleWhiteDiskClick(){
         updateDisplay();
     }
 }
-function handleBlackDiskClick(){
+function handleBlackDiskMouseDown(){
     if(turn == 2){
+        dragEl.style.backgroundImage = `var(--black-disk)`;
+        boardEl.childNodes.forEach(e => e.classList.add('mid-drag'));
         selected = -2;
         candidates = [];
         for(let i=0; i<64; i++){
@@ -273,9 +473,21 @@ function handleBlackDiskClick(){
         updateDisplay();
     }
 }
+function handleDiskMouseUp(){
+    dragEl.style.backgroundImage = '';
+    dragged && (dragged.style.backgroundSize = 'cover');
+    boardEl.childNodes.forEach(e => e.classList.remove('mid-drag'));
+    dragged = undefined;
+}
 
 // for mobile
-if(innerWidth <= 767){
+if(innerWidth / innerHeight <= 8/10){
     $('.disks').removeChild(whiteDiskEl);
     $('.mobile-only').appendChild(whiteDiskEl);
+    
+    $('.container').removeChild(blackClockEl);
+    $('.container').removeChild(whiteClockEl);
+
+    $('.disks').appendChild(blackClockEl);
+    $('.mobile-only').appendChild(whiteClockEl);
 }
